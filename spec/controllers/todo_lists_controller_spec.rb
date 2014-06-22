3193d65113a33df31986df079ed33509530afe20
Ivan <idimitrov07@gmail.com>
2014-06-22 22:05:10 +0300

    added factory girl

--- spec/controllers/todo_lists_controller_spec.rb
+++ spec/controllers/todo_lists_controller_spec.rb
@@ -30,6 +30,10 @@ describe TodoListsController do
   # TodoListsController. Be sure to keep this updated too.
   let(:valid_session) { {} }
 
+  before do
+    controller.stub(:current_user).and_return(User.new)
+  end
+
   describe "GET index" do
     it "assigns all todo_lists as @todo_lists" do
       todo_list = TodoList.create! valid_attributes
