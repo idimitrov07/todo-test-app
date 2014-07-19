6d3ab9e41376484ad48164a14a795d61c36d79fe
Ivan Dimitrov <idimitrov07@gmail.com>
2014-07-19 20:46:56 +0300

    finished test for user support

--- spec/features/todo_items/index_spec.rb
+++ spec/features/todo_items/index_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
 describe "Viewing todo items" do
-  let(:user) { create(:user) }
-  let!(:todo_list) { TodoList.create(title: "Grocery list", description: "Groceries") }
+  let(:user) { todo_list.user }
+  let!(:todo_list) { create(:todo_list) }
 
   before do
     sign_in user, password: "treehouse1"
