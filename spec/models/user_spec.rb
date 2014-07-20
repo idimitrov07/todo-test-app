5e9f81e8ac29763824580079d7028315ebe085da
Ivan Dimitrov <idimitrov07@gmail.com>
2014-07-20 19:43:15 +0300

    started password reset proccess

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -63,5 +63,18 @@ describe User do
     end
   end
 
+  describe "#generate_password_reset_token!" do
+    let(:user) { create(:user) }
+
+    it "changes the password reset token attribute" do 
+      expect { user.generate_password_reset_token! }.to change{user.password_reset_token}
+    end
+
+    it "calls SecureRandom.urlsafe_base64 to generate password_reset_token" do
+      expect(SecureRandom).to receive(:urlsafe_base64)
+      user.generate_password_reset_token!
+    end
+  end
+
 
 end
