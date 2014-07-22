6049b86e856d784cecb93070d4c4e9c37a596507
Ivan Dimitrov <idimitrov07@gmail.com>
2014-07-22 22:23:48 +0300

    started create action in password reset controller

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -4,4 +4,11 @@ class PasswordResetsController < ApplicationController
 
 	end
 
+	def create
+		user = User.find_by(email: params[:email])
+		user.generate_password_reset_token!
+		flash[:success] = "Password email sent"
+		redirect_to login_path
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Odot::Application.routes.draw do
 
   resources :users
   resources :user_sessions, only: [:new, :create]
-  resources :password_resets, only: [:new]
+  resources :password_resets, only: [:new, :create]
 
 
   resources :todo_lists do
