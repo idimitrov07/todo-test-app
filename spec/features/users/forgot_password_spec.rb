06abcb27096b1eefbd1f66013bc60297628eacec
Ivan Dimitrov <idimitrov07@gmail.com>
2014-08-22 01:54:14 +0300

    Fix tests

--- spec/features/users/forgot_password_spec.rb
+++ spec/features/users/forgot_password_spec.rb
@@ -27,12 +27,12 @@ describe "Forgotten passwords" do
 		expect(page).to have_content("password was updated")
 		expect(page.current_path).to eq(todo_lists_path)
 
-		click_link "Log Out"
+		click_link "Sign Out"
 		expect(page).to have_content("You must be logged in to access the page")
 		visit login_path
 		fill_in "Email", with: user.email
 		fill_in "Password", with: "mynewpassword1"
-		click_button "Log In"
+		click_button "Sign In"
 		expect(page).to have_content("Thanks for logging in")
 	end
 
--- spec/features/users/registration_spec.rb
+++ spec/features/users/registration_spec.rb
@@ -6,15 +6,15 @@ describe "Signing up" do
     expect(User.count).to eq(0)
 
     visit "/"
-    expect(page).to have_content("Sign up")
-    click_link "Sign up"
+    expect(page).to have_content("Sign Up")
+    within("header") { click_link "Sign Up" }
 
     fill_in "First Name", with: "Jason"
     fill_in "Last Name", with: "Seifer"
     fill_in "Email", with: "jason@teamtreehouse.com"
     fill_in "Password", with: "password1234"
     fill_in "Password (Again)", with: "password1234"
-    click_button "Sign up"
+    click_button "Sign Up"
 
     expect(User.count).to eq(1)
   end
