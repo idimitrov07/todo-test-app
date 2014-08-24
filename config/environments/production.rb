d51d0a45a209775d9f00e4b79cff4b940f2d00cb
Ivan Dimitrov <idimitrov07@gmail.com>
2014-08-24 22:35:16 +0300

    Add mailgun email engine.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,18 +80,16 @@ Odot::Application.configure do
 
   config.action_mailer.default_url_options = { host: "todolistitem.herokuapp.com"}
   config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
 
-  #config.action_mailer.smtp_settings = {
   ActionMailer::Base.smtp_settings = {
-      :address        => 'smtp.sendgrid.net',
-      :port           => '587',
-      :authentication => :plain,
-      :user_name      => ENV['SENDGRID_USERNAME'],
-      :password       => ENV['SENDGRID_PASSWORD'],
-      :domain         => 'heroku.com',
-      :enable_starttls_auto => true
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'todolistitem.heroku.com',
+    :authentication => :plain,
   }
-
   ActionMailer::Base.delivery_method = :smtp
 
 end
