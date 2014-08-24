8cab218ba6f338a353a1c0329ec5c82f500910f8
Ivan Dimitrov <idimitrov07@gmail.com>
2014-08-24 16:28:10 +0300

    Email setup try.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,19 +79,9 @@ Odot::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   config.action_mailer.default_url_options = { host: "todolistitem.herokuapp.com"}
-  # config.action_mailer.default :charset => "utf-8"
-  # config.action_mailer.perform_deliveries = true
-  # config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.smtp_settings = {
-    #ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
-    :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
-    :enable_starttls_auto => true
-  }
 end
